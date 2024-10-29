e36a8a5a57215586ffd4c600054b20f67290889f
ardi2924 <indrardi92@gmail.com>
2024-10-29 13:11:21 +0700

    fix: the icon and description of navbar

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,15 +1,25 @@
 "use client";
 
 import { NextUIProvider } from "@nextui-org/react";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 const UserContext = createContext({});
 
 export default function Providers({ children }) {
   const [loggedUser, setLoggedUser] = useState({
-    name: "Tejo Suherman",
-    greenHouse: "Green House 6",
+    name: "",
+    greenHouse: "",
   });
+
+  useEffect(() => {
+    const userData = JSON.parse(localStorage.getItem("user"));
+    console.log(userData);
+      const { id_gh, name } = userData.farmer[0];
+      setLoggedUser({
+        name: name,
+        greenHouse: `Green House ${id_gh}`,
+      });
+  }, []);
 
   return (
     <UserContext.Provider value={{ loggedUser, setLoggedUser }}>
